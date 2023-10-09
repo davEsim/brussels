da1dc907ffd667551f90be929a852ae6d641c5d4
davEsim <davxsim@gmail.com>
2023-10-09 23:59:57 +0200

    pročištěný rezrvace, tim pádem ale nefunkční :-)

---
+++ included/partials/modals/personalData.php
@@ -0,0 +1,32 @@
+﻿
+<div class="small reveal" id="modal-personalData" data-reveal>
+<button class="close-button" data-close aria-label="Close modal" type="button">
+  <span aria-hidden="true">&times;</span>
+</button>
+  <h2 id="modalTitle">Information on the processing of personal data</h2>
+  <p><strong>Personal Data Controller: Člověk v tísni, o.p.s. (People in Need)</strong>, registered seat at Šafaříkova 24, 120 00 Prague 2, ID No. 25755277, registered in the Foundations Register managed by the Municipal Court in Prague, Section O, File 119; contact details of the Data Protection Officer: <a href="mailto:dpo@clovekvtisni.cz">dpo@clovekvtisni.cz</a></p>
+<p><strong>Please note that the Controller may process your personal data to the extent stated: Basic identification and contact details.</strong></p>
+<p><strong>Source</strong> from which the processed personal data originates: <strong>Internet or other public sources</strong>.</p>
+<p>The data is processed for the <strong>purpose of invitation to the One World Festival in Brussels 2022 and related communication.</strong></p>
+<p>The legal base for the processing is the following:</p>
+<ul>
+<li>it is necessary for the purposes of the legitimate interests pursued by the controller – namely: implementation of the One World Film Festival in Brussels 2022 and related communication</li>
+</ul>
+<p>
+The Controller shall store the above-mentioned data <strong>for the duration of the One World Film Festival in Brussels 2022 and afterwards for the duration of 3 months for the purpose of a complaint or other possible follow-up communication.</strong></p>
+
+<p>The processing of personal data is done by the Controller; however, also the following processors may process personal data for the Controller: the co-organizer of the festival the Czech Center in Brussels.</p>
+<p>
+The Controller shall hand personal data over to another person in the circumstances provided for in the legislation.</p>
+<p><strong>
+Your rights:</strong></p>
+<ul>
+<li>right to request information on which personal data of yours the Controller is processing,</li>
+<li>right to request explanation from the Controller regarding the processing of personal data,</li>
+<li>right to request access to such data from the Controller, right to have the data updated, corrected or restricted, as the case may be, and right to object against processing,</li>
+<li>the right to obtain personal data in a structured, commonly used and machine-readable format, if processing is based on consent or contract,</li>
+<li>right to request the deletion of such personal data from the Controller (provided that the reason for processing is no longer valid),</li>
+<li>right to address the Controller or complain to the Office for Personal Data Protection in case of doubt regarding the compliance with the obligations related to the processing of personal data.</li>
+<ul>
+</div>
+
--- included/templates/xBrusselReservation.php
+++ included/templates/xBrusselReservation.php
@@ -1,7 +1,11 @@
 ﻿
 <?php
-//print_r($_POST);
-ini_set("display_errors",1);
+exit("ahoj");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+error_reporting(-1);
+
 $lang = $_ENV["lang"];
 $itemSeo = intval($_ENV["itemSeo"]);
 
@@ -59,42 +63,13 @@ if($_POST){?>
 								);
 				$db->query("INSERT INTO xBrusselViewers VALUES (NULL, :screenId, :fname, :sname, :fnameOfSigner, :snameOfSigner, :dateOfBirth, :idType, :idN, :nationality, :organisation, :badge, :mail, :agreeData, :agreeNewsletter, :uid, NOW(), :reminderSent)", $params);
 				
-				if($_POST["brusselViewer2SName"]){
-					$params = array(
-										":screenId" 		=> $_POST["sid"],
-										":fname"			=> $_POST["brusselViewer2FName"],
-										":sname"			=> $_POST["brusselViewer2SName"],
-										":fnameOfSigner"	=> $_POST["brusselViewerFName"],
-										":snameOfSigner"	=> $_POST["brusselViewerSName"],
-										":dateOfBirth"	=> "",
-										":idType"			=> "",
-										":idN"			=> "",
-										":nationality"	=> "",
-										":organisation"	=> $_POST["organisation"],
-										":badge"	        => "",
-										":mail"			=> "",
-										":agreeData"		=> $agreeData,
-										":agreeNewsletter"=> "ne",
-										":uid"			=> $uid,
-										":reminderSent"	=> NULL
-									);
-					$db->query("INSERT INTO xBrusselViewers VALUES (NULL, :screenId, :fname, :sname, :fnameOfSigner, :snameOfSigner, :dateOfBirth, :idType, :idN, :nationality, :organisation, :badge, :mail, :agreeData, :agreeNewsletter, :uid, NOW(), :reminderSent)", $params);
-				}
-				if($lang=="CZ"){
-					echo "	<p>Vaše registrace na projekci proběhla úspěšně.</p>
-							<p>Na vámi zadanou adresu ".$_POST["brusselViewerM"]." byl odeslán potvrzovací e-mail.</p>
-							<p class='warning'><strong>Zkontrolujte, prosím, i vaší spam složku (na nevyžádanou poštu), jestli tam náhodou mail od nás nezapadl.</strong></p>
-							<p>Děkujeme</p>
-							<a class='hollow button marginBottom' href='../brusel'><b class='fi-arrow-left'></b> Zpět na program</a>
-						";
-				}else{
-					echo "	<p>Your registration has been successful.</p>
-							<p>An email message confirming registration has been sent to your email address ".$_POST["brusselViewerM"].".</p>
-							<p class='warning'><strong>If you can not find our mail, please,  check your spam folder too.</strong></p>
-							<p>Thank you</p>
-							<a class='hollow button marginBottom' href='../brussels'><b class='fi-arrow-left'></b> Back to Programme</a>
-						";
-				}
+				echo "	<p>Your registration has been successful.</p>
+						<p>An email message confirming registration has been sent to your email address ".$_POST["brusselViewerM"].".</p>
+						<p class='warning'><strong>If you can not find our mail, please,  check your spam folder too.</strong></p>
+						<p>Thank you</p>
+						<a class='hollow button marginBottom' href='../brussels'><b class='fi-arrow-left'></b> Back to Programme</a>
+					";
+				
 				include_once("./included/partials/mails/brusselRegistration.php");
 				$mail = new PHPMailer();                                			
 				$mail->From="brussels@oneworld.cz";
@@ -138,181 +113,163 @@ if(!$_POST || $message){
                 <button class="medium hollow button button--play button--310" data-open="trailer"><?=__("Přehrát trailer")?></button> 
             <?
             }
-            
             if($activeBrusselScreeningFilm["linkToOnline$lang"]){
             ?>  
                 <a href="<?=$activeBrusselScreeningFilm["linkToOnline$lang"]?>" target="_blank" class="medium hollow button button--rightArrow button--310"><?=__("Zhlédnout online")?></a>
             <?php
-            }
-            
+            }  
             ?>
         </div>
     </div>
-	
-	  	
-		<? if($itemId != 1 && $itemId != 2){?>
-			<section class="section--neutral">
-				<div class="row">
-					<div class="medium-12 columns">
-						<h3 class="marginBottom2"><?=__("Vaše rezervace na projekci")?></h3>
-						<? if($message){?><p class="warning"><?=$message?></p><?}?>
-					</div>
-				</div>
-
-				<?php
-				if($activeBrusselScreeningCountOfViewers >= $activeBrusselScreening["countOfViewers"]){
-				?>
-					<div class="row row--textDecoration align-justify">
-						<div class="medium-6 columns">
-						<? if($lang == "CZ"){?>
-                            <p>Promítání je již plně obsazeno. <br>Pokud chcete být zařazeni na čekací listinu, kontaktujte <a href="mailto:brussels@oneworld.cz">brussels@oneworld.cz</a>.</p>
-                        <?}else{?>
-                            <p>The screening is now fully booked. <br>Contact <a href="mailto:brussels@oneworld.cz">brussels@oneworld.cz</a> if you want to be put on a&nbsp;waiting list.</p>
-                        <?}?>  
-						</div>
-					</div>
-				<?}else{
-					
-					?>
-					<div class="row align-justify" data-abide>
-						<div class="medium-6 columns">
-							<form method="post">  
-								<table class="brusselForm">
-									<tr><td><?=__("Jméno")?> <sup>&ast;</sup></td><td><input type="text" name="brusselViewerFName" size="20" required /></td></tr>
-									<tr><td><?=__("Přijmení")?> <sup>&ast;</sup></td><td><input type="text" name="brusselViewerSName" size="20" required /></td></tr>
-									<tr><td>E-mail <sup>&ast;</sup></td><td><input type="email" name="brusselViewerM" required  /></td></tr>
-									<tr><td><?=__("Organizace")?> <sup>&ast;</sup></td><td><input type="text" name="organisation" required  /></td></tr>
-									<tr class="nS"><td>E-mail</td><td><input type="email" name="email" /></td></tr>
-									<tr class="nS"><td>E-mail</td><td><input type="email" name="mail" /></td></tr>
-									<tr><td><?=__("Jméno druhé osoby")?></td><td><input type="text" name="brusselViewer2FName" /></td></tr>
-									<tr><td><?=__("Přijmení druhé osoby")?></td><td><input type="text" name="brusselViewer2SName" /></td></tr>
-									<table class="brusselForm">
-									<tr>
-										<td><input type="checkbox" name="agreeData" value="<?=__("ano")?>" required> <sup>&ast;</sup></td>
-										<td>
-											<p><a href="#" data-open="modal-personalData"><?=__("Souhlasím s tím, že mé kontaktní údaje budou po dobu konání festivalu uchovávány za účelem zasílání připomínek o konání projekcí.")?></a></p>
-										</td>
-									</tr>
-									<tr>
-										<td><input type="checkbox" name="agreeNewsletter" value="<?=__("ano")?>"></td>
-										<td>
-											<p><?=__("Chci být informován o příštím ročníku festivalu Jeden svět v Bruselu (2024).")?></p>
-										</td>
-									</tr>
-								</table>
-								<div id="formSend" data-value="<?=__("Odeslat")?>"></div>
-								<input type="hidden" name="sid" value="<?=$activeBrusselScreening["id"]?>" />
-								<input type="hidden" name="pid" value="<?=$activeBrusselScreening["id_xBrusselPlaces"]?>" />
-							</form>
-						</div>
-						
-						<div class="medium-5 columns">
-							<div style="border-left: 1px solid #cacaca; padding-left: 1rem">
-								<p class="marginBottom1"><strong><?=__("Projekce")?></strong></p>
-								<h4><?=$activeBrusselScreeningFilm["title$lang"]?></h4>
-								<p><strong><?=invertDatumFromDB($activeBrusselScreening["date"],1)." | ".$activeBrusselScreening["time"]?></strong></p>
-								<p><a href="<?=$activeBrusselScreeningPlace["url"]?>" target="_blank"><?=$activeBrusselScreeningPlace["xBrusselPlaces"]?></a><br /><?=$activeBrusselScreeningPlace["address"]?></p> 
-							</div> 
-						</div>
-					</div>
-				<?  }?>	
-			</section>
-			<?}else{?>
-				<section class="section--neutral">
-					<div class="row align-center">
-						<div class='medium-6 small-12 columns'>
-							<p class="marginBottom1"><strong><?=__("Projekce")?></strong></p>
-							<h4><?=$activeBrusselScreeningFilm["title$lang"]?></h4>
-							<p><strong><?=invertDatumFromDB($activeBrusselScreening["date"],1)." | ".$activeBrusselScreening["time"]?></strong></p>
-							<p><a href="<?=$activeBrusselScreeningPlace["url"]?>" target="_blank"><?=$activeBrusselScreeningPlace["xBrusselPlaces"]?></a><br /><?=$activeBrusselScreeningPlace["address"]?></p> 
-						</div>	
-					</div>
-				</section>		
-			<?}
-			if($activeBrusselScreeningFilm["id"]){		
-			?>
-			<div class='row marginTop4 align-center'>
-				<div class='medium-6 small-12 columns'>
-					<h2><?=__("O filmu")?></h2>
-					<p><strong><?=$activeBrusselScreeningFilm["shortSynopsis$lang"]?></strong></p>
-					<p><?=$activeBrusselScreeningFilm["synopsys$lang"]?></p>
+	<section class="section--neutral">
+		<div class="row">
+			<div class="medium-12 columns">
+				<h3 class="marginBottom2"><?=__("Vaše rezervace na projekci")?></h3>
+				<? if($message){?><p class="warning"><?=$message?></p><?}?>
+			</div>
+		</div>
+		<?php
+		if($activeBrusselScreeningCountOfViewers >= $activeBrusselScreening["countOfViewers"]){
+		?>
+			<div class="row row--textDecoration align-justify">
+				<div class="medium-6 columns">
+				<? if($lang == "CZ"){?>
+					<p>Promítání je již plně obsazeno. <br>Pokud chcete být zařazeni na čekací listinu, kontaktujte <a href="mailto:brussels@oneworld.cz">brussels@oneworld.cz</a>.</p>
+				<?}else{?>
+					<p>The screening is now fully booked. <br>Contact <a href="mailto:brussels@oneworld.cz">brussels@oneworld.cz</a> if you want to be put on a&nbsp;waiting list.</p>
+				<?}?>  
 				</div>
 			</div>
-			<div class="row marginTop credits--filmDetail"> 
-				<div class='medium-4 small-12 columns smaller'>
-					<h6><?=__("Název")?></h6>
-					<p><?=$activeBrusselScreeningFilm["title$lang"]?></p>
-
-					<h6 class="marginTop2"><?=__("Originální název")?></h6>
-					<p><?=$activeBrusselScreeningFilm["TITLE_ORIGINAL"]?></p>
-					<h6><?=__("Originální znění")?></h6>
-					<p>
-						<?
-						if($clickedScreen["language$lang"]){ //bud zobrazuji jazyk z projekce, pokud není vyplněn, zobrazuji jazyk vyplněný u filmu
-							echo $clickedScreen["language$lang"];
-						}else{
-							echo $activeBrusselScreeningFilm["language$lang"];
-						}
-						?>
-					</p>
-					<h6><?=__("Titulky")?></h6>
-					<p>
-						<?php 
-						if($clickedScreen["subtitles$lang"]){
-							echo $clickedScreen["subtitles$lang"];
-						}else{
-							echo $activeBrusselScreeningFilm["subtitles$lang"];
-						}
-						?>
-					</p>
-					
-					<?if($activeBrusselScreeningFilm["premiere$lang"]){?>
-						<h6><?=__("Premiéra")?></h6>
-						<p><?=$activeBrusselScreeningFilm["premiere$lang"]?></p>
-					<?}?>
+		<?}else{
+			
+			?>
+			<div class="row align-justify" data-abide>
+				<div class="medium-6 columns">
+					<form method="post">  
+						<table class="brusselForm">
+							<tr><td><?=__("Jméno")?> <sup>&ast;</sup></td><td><input type="text" name="brusselViewerFName" size="20" required /></td></tr>
+							<tr><td><?=__("Přijmení")?> <sup>&ast;</sup></td><td><input type="text" name="brusselViewerSName" size="20" required /></td></tr>
+							<tr><td>E-mail <sup>&ast;</sup></td><td><input type="email" name="brusselViewerM" required  /></td></tr>
+							<tr><td><?=__("Organizace")?> <sup>&ast;</sup></td><td><input type="text" name="organisation" required  /></td></tr>
+							<tr class="nS"><td>E-mail</td><td><input type="email" name="email" /></td></tr>
+							<tr class="nS"><td>E-mail</td><td><input type="email" name="mail" /></td></tr>
+							<tr><td><?=__("Jméno druhé osoby")?></td><td><input type="text" name="brusselViewer2FName" /></td></tr>
+							<tr><td><?=__("Přijmení druhé osoby")?></td><td><input type="text" name="brusselViewer2SName" /></td></tr>
+							<table class="brusselForm">
+							<tr>
+								<td><input type="checkbox" name="agreeData" value="<?=__("ano")?>" required> <sup>&ast;</sup></td>
+								<td>
+									<p><a href="#" data-open="modal-personalData"><?=__("Souhlasím s tím, že mé kontaktní údaje budou po dobu konání festivalu uchovávány za účelem zasílání připomínek o konání projekcí.")?></a></p>
+								</td>
+							</tr>
+							<tr>
+								<td><input type="checkbox" name="agreeNewsletter" value="<?=__("ano")?>"></td>
+								<td>
+									<p><?=__("Chci být informován o příštím ročníku festivalu Jeden svět v Bruselu (2024).")?></p>
+								</td>
+							</tr>
+						</table>
+						<div id="formSend" data-value="<?=__("Odeslat")?>"></div>
+						<input type="hidden" name="sid" value="<?=$activeBrusselScreening["id"]?>" />
+						<input type="hidden" name="pid" value="<?=$activeBrusselScreening["id_xBrusselPlaces"]?>" />
+					</form>
+				</div>
 				
+				<div class="medium-5 columns">
+					<div style="border-left: 1px solid #cacaca; padding-left: 1rem">
+						<p class="marginBottom1"><strong><?=__("Projekce")?></strong></p>
+						<h4><?=$activeBrusselScreeningFilm["title$lang"]?></h4>
+						<p><strong><?=invertDatumFromDB($activeBrusselScreening["date"],1)." | ".$activeBrusselScreening["time"]?></strong></p>
+						<p><a href="<?=$activeBrusselScreeningPlace["url"]?>" target="_blank"><?=$activeBrusselScreeningPlace["xBrusselPlaces"]?></a><br /><?=$activeBrusselScreeningPlace["address"]?></p> 
+					</div> 
 				</div>
-				<div class="medium-4 small-12 columns smaller">
-					<h6><?=__("Rok výroby")?></h6>
-					<p><?=$activeBrusselScreeningFilm["YEAR"]?></p>
-					<h6><?=__("Země původu")?></h6>
-					<p><?=$activeBrusselScreeningFilm["country$lang"]?></p>
-					<h6><?=__("Stopáž")?></h6>
-					<p><?=$activeBrusselScreeningFilm["time$lang"]?> min.</p>
-					
-					<!--
-					<h6><?=__("Tagy")?></h6>
-					<p>
-						<?php
-							/*
-							$tags = $films->findItemsInRelations($activeBrusselScreeningFilm["id"], "xFilmTags");
-							$i = 0;
-							foreach($tags AS $tag){
-								if($i++) echo ", ";
-								echo $tag["tag$lang"];
-								
-							}
-							*/
-						?>
-					</p>
-					-->	
-					
-				</div>
-				<div class="medium-3 small-12 columns smaller">
-					
-						<?php
-						$filmDirectors = $filmDirectors->listingWhereLike("id_xFilms", $activeBrusselScreeningFilm["id"], "id", "ASC", 0, 3);
-						foreach($filmDirectors AS $filmDirector){?>
-							<img src="<?=$filmDirector["imageUrl"]?>" alt="<?=$filmDirector["fName"]?> <?=$filmDirector["sName"]?>">
-							<h6><?=__("Režie")?></h6>
-							<h5><?=$filmDirector["fName"]?> <?=$filmDirector["sName"]?></h5>
-							<p><?=$filmDirector["fgraphy$lang"]?></p>
-						<?}
-						?>
-				</div>
-			</div>   
+			</div>
+		<?  }?>	
+	</section>
+	<?
+	if($activeBrusselScreeningFilm["id"]){		
+	?>
+	<div class='row marginTop4 align-center'>
+		<div class='medium-6 small-12 columns'>
+			<h2><?=__("O filmu")?></h2>
+			<p><strong><?=$activeBrusselScreeningFilm["shortSynopsis$lang"]?></strong></p>
+			<p><?=$activeBrusselScreeningFilm["synopsys$lang"]?></p>
+		</div>
+	</div>
+	<div class="row marginTop credits--filmDetail"> 
+		<div class='medium-4 small-12 columns smaller'>
+			<h6><?=__("Název")?></h6>
+			<p><?=$activeBrusselScreeningFilm["title$lang"]?></p>
+
+			<h6 class="marginTop2"><?=__("Originální název")?></h6>
+			<p><?=$activeBrusselScreeningFilm["TITLE_ORIGINAL"]?></p>
+			<h6><?=__("Originální znění")?></h6>
+			<p>
 				<?
-			}
+				if($clickedScreen["language$lang"]){ //bud zobrazuji jazyk z projekce, pokud není vyplněn, zobrazuji jazyk vyplněný u filmu
+					echo $clickedScreen["language$lang"];
+				}else{
+					echo $activeBrusselScreeningFilm["language$lang"];
+				}
+				?>
+			</p>
+			<h6><?=__("Titulky")?></h6>
+			<p>
+				<?php 
+				if($clickedScreen["subtitles$lang"]){
+					echo $clickedScreen["subtitles$lang"];
+				}else{
+					echo $activeBrusselScreeningFilm["subtitles$lang"];
+				}
+				?>
+			</p>
+			
+			<?if($activeBrusselScreeningFilm["premiere$lang"]){?>
+				<h6><?=__("Premiéra")?></h6>
+				<p><?=$activeBrusselScreeningFilm["premiere$lang"]?></p>
+			<?}?>
+		
+		</div>
+		<div class="medium-4 small-12 columns smaller">
+			<h6><?=__("Rok výroby")?></h6>
+			<p><?=$activeBrusselScreeningFilm["YEAR"]?></p>
+			<h6><?=__("Země původu")?></h6>
+			<p><?=$activeBrusselScreeningFilm["country$lang"]?></p>
+			<h6><?=__("Stopáž")?></h6>
+			<p><?=$activeBrusselScreeningFilm["time$lang"]?> min.</p>
+			
+			<!--
+			<h6><?=__("Tagy")?></h6>
+			<p>
+				<?php
+					/*
+					$tags = $films->findItemsInRelations($activeBrusselScreeningFilm["id"], "xFilmTags");
+					$i = 0;
+					foreach($tags AS $tag){
+						if($i++) echo ", ";
+						echo $tag["tag$lang"];
+						
+					}
+					*/
+				?>
+			</p>
+			-->	
+			
+		</div>
+		<div class="medium-3 small-12 columns smaller">
+			
+				<?php
+				$filmDirectors = $filmDirectors->listingWhereLike("id_xFilms", $activeBrusselScreeningFilm["id"], "id", "ASC", 0, 3);
+				foreach($filmDirectors AS $filmDirector){?>
+					<img src="<?=$filmDirector["imageUrl"]?>" alt="<?=$filmDirector["fName"]?> <?=$filmDirector["sName"]?>">
+					<h6><?=__("Režie")?></h6>
+					<h5><?=$filmDirector["fName"]?> <?=$filmDirector["sName"]?></h5>
+					<p><?=$filmDirector["fgraphy$lang"]?></p>
+				<?}
+				?>
+		</div>
+	</div>   
+<?			
 }
 ?>
 
