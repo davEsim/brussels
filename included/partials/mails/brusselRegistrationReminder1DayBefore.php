245faef70a243ed7da31ea35914216a76c27d5fe
davEsim <davxsim@gmail.com>
2023-11-02 10:09:39 +0100

    del link v reminderech

--- included/partials/mails/brusselRegistrationReminder1DayBefore.php
+++ included/partials/mails/brusselRegistrationReminder1DayBefore.php
@@ -1,6 +1,9 @@
 ﻿<?php
+$delRegistrationLink = $_ENV["host"]."/brussel-cancel-reservation/".$uid;
+
 $body = "<p>Dear ".$r["fname"].",</p>";
 $body .= "<p>The screening of ".$r["film"]." takes place tomorrow at ".$r["time"].", ".$r["xBrusselPlaces"].", ".$r["address"].".</p>";
+$body.="<p><strong>Please, nebude moci, zruste svoji registraci kliknutim na tento odkaz<br>
+<a title='cancel registration' href='".$delRegistrationLink."'>".$delRegistrationLink."</a></strong></p>";
 $body .= "<p>We are looking forward to meeting you there!</p>";
 $body .= "<p>The One World in Brussels Team</p>";
-?>
